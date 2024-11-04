7e7dc0ee2a1ec59ce0d76534676380f8db71a6bb
Junior's Signature <162041570+juniorssignature21@users.noreply.github.com>
2024-11-04 21:31:37 +0100

    Update settings.py

--- nexgigproj/settings.py
+++ nexgigproj/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-        "django.middleware.security.SecurityMiddleware",
+    "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware", # newly added
 
 
